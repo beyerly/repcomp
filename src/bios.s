a822544543f5ceacb2e113f3af946de54513bc8d
Robert de Gruijl <robertdegruijl@gmail.com>
2025-05-26 14:27:39 -0700

    initial

--- src/bios.s
+++ src/bios.s
@@ -1,16 +1,66 @@
 .org $8000
 
+
+PORTB = $6000 ; VIA PORTB
+PORTA = $6001 ; VIA PORTA
+PORTBDDR = $6002 ; VIA PORTB Data Direction
+PORTADDR = $6003 ; VIA PORTA Data Direction
+AUXCTRL = $600B ; VIA Auxilary Control Register
+KEYPRESSED = #$01
+DELAY = #$ff
+
 reset:
  lda #$ff
- sta $6002
+ sta PORTBDDR ; VIA Port B all outputs
+ lda #$00
+ sta PORTADDR ; VIA Port A all inputs
+ lda #00
+ sta AUXCTRL ; VIA PORT A latching disabled
 
-loop:
+
+pattern0:
  lda #$55
- sta $6000
-
+ sta PORTB ; LED pattern 1
+ ldx DELAY
+wait0:
+ lda PORTA
+ cmp KEYPRESSED
+ beq pattern1
+ dex
+ bne wait0
  lda #$aa
- sta $6000
- jmp loop
+ sta PORTB  ; LED pattern 1
+ ldx DELAY
+wait1:
+ lda PORTA
+ cmp KEYPRESSED
+ beq pattern1
+ dex
+ bne wait1
+ jmp pattern0
+
+
+pattern1:
+ lda #$FF
+ sta PORTB ; LED pattern 1
+
+ ldx #$ff
+wait2:
+ lda PORTA
+ cmp KEYPRESSED
+ beq pattern0
+ dex
+ bne wait2
+ lda #$aa
+ sta PORTB  ; LED pattern 1
+ ldx DELAY
+wait3:
+ lda PORTA
+ cmp KEYPRESSED
+ beq pattern0
+ dex
+ bne wait3
+ jmp pattern1
 
  .org $fffc
  .word reset
